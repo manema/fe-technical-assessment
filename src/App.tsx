0181723e1672662f375a535548f682a0b557214d
Emanuel Galbarini <emanuel@rootstrap.com>
2024-05-27 17:12:06 -0300

    Adding airops sdk

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from 'react';
+import AirOps from "@airops/airops-js";
+
+const airopsInstance = new AirOps();
+
 function App() {
+
+
   return (
     <div className="text-lg">
       Technical assessment
